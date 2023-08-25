222c3f52b101f101f67ac532ce303101adcb0a33
Harenabs21 <hei.harena.6@gmail.com>
2023-08-25 17:54:44 +0300

    fix:update the pom and database instance

--- src/main/java/config/DatabaseConnection.java
+++ src/main/java/config/DatabaseConnection.java
@@ -1,11 +1,12 @@
 package config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+
+import org.springframework.stereotype.Component;
 
 
 import java.sql.*;
-@Configuration
+@Component
 public class DatabaseConnection {
     @Value("${PG_URL}")
     private String url;
