f8c7bc4de653b12ce6b142ec55252c8a53963ee4
Harenabs21 <hei.harena.6@gmail.com>
2023-08-26 19:42:13 +0300

    fix:add logical actions for the update method

--- src/main/java/Service/PostService.java
+++ src/main/java/Service/PostService.java
@@ -8,10 +8,11 @@ import org.springframework.stereotype.Service;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
-
+@AllArgsConstructor
 @Getter
 @Service
 public class PostService {
+
     private PostRepository postRepository;
     public List<Post> displayAllPosts() throws SQLException {
         return postRepository.findAll();
@@ -23,16 +24,26 @@ public class PostService {
         return postRepository.findById(id);
     }
     public void addNewPost(Post insert) throws SQLException {
-        if(postRepository.findById(insert.getId_account().getId()).isEmpty()){
+        if(postRepository.findById(insert.getId_account()).isEmpty()){
             System.out.println("user provided doesn't exist");
         }
         postRepository.insert(insert);
     }
-    public void updatePost(int id) throws SQLException {
-        if(postRepository.findById(id).isEmpty()){
+    public void updatePost(int id,Post post) throws SQLException {
+        Optional<Post> existingPostOptional = postRepository.findById(id);
+        if(existingPostOptional.isEmpty()){
             System.out.println("post provided doesn't exist");
         }
-        postRepository.updateById(id);
+        else{
+            Post existingPost = existingPostOptional.get();
+            if(post.getPostPhoto() != null){
+                existingPost.setPostPhoto(post.getPostPhoto());
+            }
+            if(post.getPostContent() != null){
+                existingPost.setPostContent(post.getPostContent());
+            }
+            postRepository.updateById(id,existingPost);
+        }
     }
     public void deletePost(int id) throws SQLException {
         if(postRepository.findById(id).isEmpty()){
