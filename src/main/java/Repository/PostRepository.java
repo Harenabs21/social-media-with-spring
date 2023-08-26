56759b22a5bb63c75ea4807fab420de1a7819502
Harenabs21 <hei.harena.6@gmail.com>
2023-08-26 19:26:55 +0300

    fix:add second parameter in updateById

--- src/main/java/Repository/PostRepository.java
+++ src/main/java/Repository/PostRepository.java
@@ -1,8 +1,6 @@
 package Repository;
 
 import Model.Post;
-import Model.User;
-import lombok.*;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Repository;
 
@@ -13,10 +11,8 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-@Getter
 @Repository
 public class PostRepository extends GenericRepository<Post> {
-    private Post posts;
     public PostRepository(Connection connection){
         super(connection);
     }
@@ -24,7 +20,7 @@ public class PostRepository extends GenericRepository<Post> {
     public void insert(@NotNull Post toInsert) throws SQLException {
         String sql = "INSERT INTO post(id_account,posting_date,posting_time,post_content,post_photo) VALUES(?,?,?,?,?) ";
         try (PreparedStatement statement = getConnection().prepareStatement(sql)) {
-            statement.setInt(1, toInsert.getId_account().getId());
+            statement.setInt(1, toInsert.getId_account());
             statement.setDate(2, Date.valueOf(toInsert.getPostingDate()));
             statement.setTime(3, Time.valueOf(toInsert.getPostingTime()));
             statement.setString(4, toInsert.getPostContent());
@@ -37,7 +33,7 @@ public class PostRepository extends GenericRepository<Post> {
     @Override
     public List<Post> findAll() throws SQLException {
         List<Post> AllPosts = new ArrayList<>();
-        String sql = "SELECT a.nickname, p.posting_date, p.posting_time, p.post_content, p.post_photo FROM post p INNER JOIN account a ON p.id_account = a.id";
+        String sql = "SELECT * FROM post";
         try(PreparedStatement statement = getConnection().prepareStatement(sql)){
             ResultSet resultSet = statement.executeQuery();
             while(resultSet.next()){
@@ -50,11 +46,11 @@ public class PostRepository extends GenericRepository<Post> {
     }
 
     @Override
-    public void updateById(int id) throws SQLException {
+    public void updateById(int id,Post object) throws SQLException {
         String sql = "UPDATE post SET post_content = ?, post_photo = ? WHERE id = ?";
         try(PreparedStatement statement = getConnection().prepareStatement(sql)){
-            statement.setString(1,posts.getPostContent());
-            statement.setString(2,posts.getPostPhoto());
+            statement.setString(1,object.getPostContent());
+            statement.setString(2,object.getPostPhoto());
             statement.setInt(3,id);
             statement.executeUpdate();
         }
@@ -64,11 +60,7 @@ public class PostRepository extends GenericRepository<Post> {
 
     @Override
     public Optional<Post> findById(int id) throws SQLException {
-        String sql = """
-                SELECT a.nickname, p.posting_date, p.posting_time
-                , p.post_content, p.post_photo FROM
-                post p INNER JOIN account a ON p.id_account = a.id WHERE
-                 p.id_account = ? ORDER BY posting_date DESC""";
+        String sql = "SELECT * FROM post WHERE id_account = ? ORDER BY posting_date DESC";
         try(PreparedStatement statement = getConnection().prepareStatement(sql)){
             statement.setInt(1,id);
             ResultSet resultSet = statement.executeQuery();
@@ -90,13 +82,12 @@ public class PostRepository extends GenericRepository<Post> {
 
     private Post extractPostFromResultSet(@NotNull ResultSet resultSet) throws SQLException{
         int postId = resultSet.getInt("id");
+        int accountId = resultSet.getInt("id_account");
         LocalDate postingDate = resultSet.getDate("posting_date").toLocalDate();
         LocalTime postingTime = resultSet.getTime("posting_time").toLocalTime();
         String postContent = resultSet.getString("post_content");
         String postPhoto = resultSet.getString("post_photo");
-        String nickname = resultSet.getString("nickname");
-        User username = new User(nickname);
-        return new Post(postId,username,postingDate,postingTime,postContent,postPhoto);
+        return new Post(postId,accountId,postingDate,postingTime,postContent,postPhoto);
     }
 }
 
