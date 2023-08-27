8f13c2fea9a69bf5ed64a1a523573b588470c6d4
Harenabs21 <hei.harena.6@gmail.com>
2023-08-27 22:03:31 +0300

    fix:refactor all methods in SpecificRepository

--- src/main/java/Repository/SpecificRepository.java
+++ src/main/java/Repository/SpecificRepository.java
@@ -4,13 +4,15 @@ import lombok.*;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public abstract class SpecificRepository<T> {
     private Connection connection;
+    public abstract List<T> getAllReactions(int idPost);
     public abstract void insertNewContent (T insert) throws SQLException;
-    public abstract void updateByID(T object) throws SQLException;
-    public abstract void deleteByID(T object) throws SQLException;
+    public abstract void updateReactions(int idUser,int idPost, String newReaction) throws SQLException;
+    public abstract void deleteByID(int idUser,int idPost) throws SQLException;
 }
