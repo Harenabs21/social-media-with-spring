1aa4182a39c45086a9e7aaf5031993bbb8a3ec67
Harenabs21 <hei.harena.6@gmail.com>
2023-08-27 23:06:00 +0300

    feat:implement all endpoint for the ReactPostController

--- src/main/java/Controller/ReactPostController.java
+++ src/main/java/Controller/ReactPostController.java
@@ -1,13 +1,33 @@
 package Controller;
 
+import Model.ReactPost;
 import Service.ReactPostService;
 import lombok.*;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.util.List;
 
 @AllArgsConstructor
 @Getter
-@Controller
+@RestController
+@RequestMapping(path = "/reactions")
 public class ReactPostController {
     private ReactPostService reactPostService;
-
+    @GetMapping("/{id}")
+    public List<ReactPost> displayAllReactionsOfGivenPost(@PathVariable int id) throws SQLException {
+        return reactPostService.displayReactionsOfPost(id);
+    }
+    @PostMapping("/new")
+    public void addNewReactions(@RequestBody ReactPost reactions) throws SQLException {
+        reactPostService.addNewReaction(reactions);
+    }
+    @PutMapping("/update/")
+    public void changeReactions(@RequestParam int userId, @RequestParam int postId, @RequestBody String newReaction) throws SQLException {
+        reactPostService.updateReactionOfPost(userId,postId,newReaction);
+    }
+    @DeleteMapping("/")
+    public void deleteReaction(int userId, int postId) throws SQLException {
+        reactPostService.deleteReactionOfPost(userId,postId);
+    }
 }
