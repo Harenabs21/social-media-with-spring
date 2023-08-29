b8f202052cbe65f54e7b2006127c915f94a6e12a
Harenabs21 <hei.harena.6@gmail.com>
2023-08-29 20:07:36 +0300

    feat:add new method delete  post with userId in service

--- src/main/java/Service/PostService.java
+++ src/main/java/Service/PostService.java
@@ -1,8 +1,10 @@
 package Service;
 
 import Model.Post;
+import Model.User;
 import Repository.PostRepository;
 import Repository.ReactPostRepository;
+import Repository.UserRepository;
 import lombok.*;
 import org.springframework.stereotype.Service;
 
@@ -13,17 +15,18 @@ import java.util.Optional;
 @Getter
 @Service
 public class PostService {
-
+    private UserRepository userRepository;
     private PostRepository postRepository;
     private ReactPostRepository reactPostRepository;
     public List<Post> displayAllPosts() throws SQLException {
         return postRepository.findAll();
     }
-    public Optional<Post> displayPostOfUser(int id) throws SQLException {
-        if(postRepository.findById(id).isEmpty()){
+    public List<Post> displayPostOfUser(int id) throws SQLException {
+        Optional<User> user = userRepository.findById(id);
+        if(user.isEmpty()){
             throw new ResourceNotFoundException("User not found with id"+id);
         }
-        return postRepository.findById(id);
+        return postRepository.findByIdAccount(id);
     }
     public void addNewPost(Post insert) throws SQLException {
         if(postRepository.findById(insert.getId_account()).isEmpty()){
@@ -32,13 +35,13 @@ public class PostService {
         postRepository.insert(insert);
     }
     public Optional<Post> getByIdPost(int id) throws SQLException{
-        if(postRepository.findByIdPost(id).isEmpty()){
+        if(postRepository.findById(id).isEmpty()){
             throw new ResourceNotFoundException("Post not found with id"+id);
         }
-        return postRepository.findByIdPost(id);
+        return postRepository.findById(id);
     }
     public void updatePost(int id,Post post) throws SQLException {
-        Optional<Post> existingPostOptional = postRepository.findByIdPost(id);
+        Optional<Post> existingPostOptional = postRepository.findById(id);
         if(existingPostOptional.isEmpty()){
             throw new ResourceNotFoundException("Post not found with id"+id);
         }
@@ -54,10 +57,17 @@ public class PostService {
         }
     }
     public void deletePost(int id) throws SQLException {
-        if(postRepository.findByIdPost(id).isEmpty()){
+        if(postRepository.findById(id).isEmpty()){
             throw new ResourceNotFoundException("Post not found with id"+id);
         }
         reactPostRepository.deleteByIdPost(id);
         postRepository.deleteById(id);
+    }
+    public void deleteAllPost(int id) throws SQLException{
+        Optional<User> user = userRepository.findById(id);
+        if(user.isEmpty()){
+            throw new ResourceNotFoundException("User not found with id"+id);
+        }
+         postRepository.deleteByIdUser(id);
     }
 }
